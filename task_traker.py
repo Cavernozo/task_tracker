8f542161e20c076dfa58ff67a91dfce45099ab7d
matheus rodrigues dos santos <matheuzo_dev@hotmail.com>
2025-07-13 13:51:46 -0300

    implementando Poo

--- task_traker.py
+++ task_traker.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 
+class Task():
+    def __init__(self, nome, data_ini, status, data_upd, descricao):
+        self.nome = nome
+        self.data_ini = data_ini
+        self.status = status
+        self.data_upd = data_upd
+        self.descricao = descricao
+
 app = Flask(__name__)
 
 @app.route("/index")
 def ola():
-    return render_template('main.html', titulo='Task_Tracker')
+    task1 = Task('Python', '13/07/2025', 'in-progress', '13/07/2025', 'Aprendendo sobre flask')
+    task2 = Task('sql', '13/07/2025', 'in-progress', '13/07/2025', 'Aprendendo CRUD')
+    task = [task1, task2]
+    return render_template('main.html', titulo='Task_Tracker' ,tasks= task)
 
 
 app.run(debug=True)
