be40e27f15e5888a87811c1025dad9b38d62de66
matheus rodrigues dos santos <matheuzo_dev@hotmail.com>
2025-07-14 17:16:43 -0300

    adicionando tasks

--- task_traker.py
+++ task_traker.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Task():
     def __init__(self, nome, data_ini, status, data_upd, descricao):
@@ -8,14 +8,32 @@ class Task():
         self.data_upd = data_upd
         self.descricao = descricao
 
+task1 = Task('Python', '13/07/2025', 'in-progress', '13/07/2025', 'Aprendendo sobre flask')
+task2 = Task('sql', '13/07/2025', 'in-progress', '13/07/2025', 'Aprendendo CRUD')
+lista = [task1, task2]
+
+
 app = Flask(__name__)
 
 @app.route("/index")
 def ola():
-    task1 = Task('Python', '13/07/2025', 'in-progress', '13/07/2025', 'Aprendendo sobre flask')
-    task2 = Task('sql', '13/07/2025', 'in-progress', '13/07/2025', 'Aprendendo CRUD')
-    task = [task1, task2]
-    return render_template('main.html', titulo='Task_Tracker' ,tasks= task)
 
+    return render_template('main.html', titulo='Task_Tracker' ,tasks= lista)
+
+@app.route("/novo")
+def novo():
+    return render_template("novo.html", titulo = "New Task")
+
+@app.route("/criar")
+def criar():
+    nome = request.form['nome']
+    data_ini = request.form['data_ini']
+    status = request.form['status']
+    data_upd = request.form['data_upd']
+    descricao = request.form['descricao']
+    task = Task(nome, data_ini, status, data_upd, descricao)
+
+    lista.append(task)
+    return render_template('main.html', titulo='Task_Tracker', tasks = lista)
 
 app.run(debug=True)
